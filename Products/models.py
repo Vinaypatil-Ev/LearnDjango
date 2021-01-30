963107f0d00dfc8b590a4fd58252d25a16310870
Vinaypatil-Ev <vinaypatil153@gmail.com>
2021-01-30 15:39:50 +0530

    30 jan 2021

--- Products/models.py
+++ Products/models.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     product_name = models.TextField()
     product_desc = models.TextField()
     product_price = models.TextField()
+    product_bool = models.BooleanField(default=True)
     
     def __str__(self):
         return self.product_name
